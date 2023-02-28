dc73b7666f4cbb2a98cf9702c02d2d5f70d42160
romanukalexandr <romanukalexandr@gmail.com>
2023-03-01 00:52:11 +0300

    add Task41

---
+++ Task41/Program.cs
@@ -0,0 +1,42 @@
+﻿void FillArray (int[] mas)
+{
+    for (int i = 0; i < mas.Length; i++)
+    {
+        Console.WriteLine ($"Введите {i+1}-е число");
+        mas[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+ 
+ void PrintArray (int[] arr)
+{
+    for (int j = 0; j < arr.Length; j++)
+    {
+        Console.Write(arr[j] + " ");
+    }
+}
+
+ int AboveZero (int[] massiv)
+{
+    int count = 0;
+    for (int k = 0; k < massiv.Length; k++)
+    {
+        if (massiv[k] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine ("Сколько чисел будете вводить?");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int [] array = new int[m];
+
+FillArray (array);
+Console.WriteLine ();
+
+PrintArray (array);
+Console.WriteLine ();
+
+Console.WriteLine ("Количество введенных чисел больше 0 = "+AboveZero(array));
